537bccbc6678f16a49ab23a2787d0cbf1ee8ae3e
Martijn Courteaux <courteauxmartijn@gmail.com>
2016-11-29 15:15:05 +0100

    Cachelines

---
+++ non_atomic_cacheline/README.md
@@ -0,0 +1,7 @@
+Demonstrates that writing across the boundaries of a cacheline is not atomic,
+and therefor shear reads can happen.
+
+## Usage
+
+    make
+    ./cachelines
---
+++ non_atomic_cacheline/cachelines.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <memory>
+#include <thread>
+
+volatile int stop = 0;
+
+void writer(volatile int *array, int offset) {
+    volatile int *ptr = reinterpret_cast<volatile int *>(
+        reinterpret_cast<volatile char *>(array) + offset);
+    while (!stop) {
+        ptr[0] = 0;
+        ptr[0] = -1;
+    }
+}
+
+void reader(volatile int *array, int offset) {
+    volatile int *ptr = reinterpret_cast<volatile int *>(
+        reinterpret_cast<volatile char *>(array) + offset);
+    int loops = 0;
+    for (int i = 0; i < 10; loops++) {
+        int copy = ptr[0];
+        if (copy != 0 && copy != -1) {
+            i++;
+            printf("Found a shear read: %x\n", copy);
+        }
+    }
+    printf("Loops needed: %d\n", loops);
+}
+
+int main() {
+    const int CACHELINES = 3;
+    int array[64 * CACHELINES / sizeof(int)];
+    memset(array, 0, sizeof(array));
+
+    int mod64 = ((long long unsigned int)(void *)array) % 64;
+    printf("Array:\n");
+    printf("  addr = %p\n", array);
+    printf("  size = %zu\n", sizeof(array));
+    printf("  addr %% 64 = %d\n", mod64);
+    printf("  62 - (addr %% 64) = %d\n", 62 - mod64);
+    printf("\n");
+
+    int offset = 62 - mod64;
+    printf("Enter a byte offset to start writing in the array. In order \n");
+    printf("to write over two a cacheline boundary, enter %d\n", offset);
+    printf("Byte offset: ");
+    scanf("%d", &offset);
+
+    int *ptr = array;
+    std::thread tw(writer, ptr, offset);
+    std::thread tr(reader, ptr, offset);
+    tr.join();
+    stop = 1;
+    tw.join();
+
+    printf("Result: (16 bytes per line)\n");
+    // Print the memory byte by byte
+    char *byte_ptr = reinterpret_cast<char *>(ptr);
+    for (int i = 0; i < CACHELINES * 4; ++i) {
+        for (int k = 0; k < 16; ++k) {
+            char *a = &byte_ptr[i * 16 + k];
+            // Color the cachelines alternatively
+            if (((((long long unsigned int)(void *)a) / 64) & 1) == 0) {
+                printf("\033[31m");
+            } else {
+                printf("\033[34m");
+            }
+            printf(" %02x", (*a) & 0xff);
+        }
+        printf("\n");
+    }
+    printf("\033[39;49m");
+}
